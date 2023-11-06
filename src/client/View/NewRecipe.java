e6f9f697b86d63db1bcadaaed007ff6d21d3fa6f
ChristophianSulaiman <114318832+ChristophianSulaiman@users.noreply.github.com>
2023-11-06 07:08:20 +0700

    UI
    Added Main UI panel and microphone panel

---
+++ src/client/View/NewRecipe.java
@@ -0,0 +1,76 @@
+package client.View;
+
+import javafx.geometry.Pos;
+import javafx.application.Application;
+import javafx.stage.Stage;
+import javafx.scene.Scene;
+import javafx.scene.control.*;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.*;
+import javafx.scene.text.*;
+
+public class NewRecipe extends Application {
+
+    @Override
+    public void start(Stage primaryStage) {
+        // Create the main layout for the "New Recipe Setup" panel
+        VBox root = new VBox();
+        root.setSpacing(20);
+        root.setAlignment(Pos.CENTER);
+        root.setStyle("-fx-background-color: #F0F8FF;");
+
+        // Add a title at the top
+        Text title = new Text("New Recipe Setup");
+        title.setStyle("-fx-font-weight: bold; -fx-font-size: 20;");
+        root.getChildren().add(title);
+
+        // Create a container for the setup options
+        VBox setupContainer = new VBox();
+        setupContainer.setSpacing(10);
+
+        //PROMPT 1
+        HBox option1Container = new HBox(10);
+        Label option1Label = new Label("1) Breakfast, Lunch, or Dinner?");
+        option1Label.setStyle("-fx-font-size: 16;"); // Increase the font size
+        Button microphoneButton1 = new Button();
+        Image microphoneImage = new Image(getClass().getResourceAsStream(".\\microphone.png"));
+
+        ImageView microphoneImageView1 = new ImageView(microphoneImage);
+        microphoneImageView1.setFitHeight(30);
+        microphoneImageView1.setFitWidth(30);
+        microphoneButton1.setGraphic(microphoneImageView1);
+        option1Container.getChildren().addAll(option1Label, microphoneButton1);
+
+        // PROMPT 2
+        HBox option2Container = new HBox(10);
+        Label option2Label = new Label("2) List the ingredients you have");
+        option2Label.setStyle("-fx-font-size: 16;"); // Increase the font size
+        Button microphoneButton2 = new Button();
+        ImageView microphoneImageView2 = new ImageView(microphoneImage);
+        microphoneImageView2.setFitHeight(30);
+        microphoneImageView2.setFitWidth(30);
+        microphoneButton2.setGraphic(microphoneImageView2);
+        option2Container.getChildren().addAll(option2Label, microphoneButton2);
+
+        setupContainer.getChildren().addAll(option1Container, option2Container);
+
+        // Add the setup container to the main layout
+        root.getChildren().add(setupContainer);
+
+        // Create a button to save the setup or perform any action
+        Button saveButton = new Button("Save Setup");
+        root.getChildren().add(saveButton);
+
+        // Create a scene and set it on the stage
+        Scene scene = new Scene(root, 700, 600);
+        primaryStage.setTitle("New Recipe Setup");
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
+
